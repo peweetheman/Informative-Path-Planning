a6c3b2a68f224128e1c0f09c61e3d6afb6b0a61c
K-Eusemann <eusemann.kevin@gmail.com>
2019-08-12 10:32:48 +0200

    improved boundary use

--- Config.py
+++ Config.py
@@ -28,10 +28,11 @@ v_auv = 1.0  # AUV velocity in meter/second (constant)
 field_dim = [0, 9.9, 0, 4.9]  # x_min , x_max, y_min, y_max
 
 # Simulation variables
-plot = False      # whether or not to plot while running
+plot = True  # whether or not to plot while running
+collect_data = False
 sigma_w_squ = 0.2 ** 2  # Measurement variance
 sample_time_gmrf = 100  # Sample/Calculation time in ms of GMRF algorithm, not used right now
-simulation_end_time = 2000000 # Run time of simulation in ms, not used right now
+simulation_end_time = 2000000  # Run time of simulation in ms, not used right now
 
 """Choose GMRF parameters"""
 gmrf_dim = [50, 25, 15, 15]  # lxf, lyf, dvx, dvy
@@ -80,36 +81,36 @@ elif set_GMRF_cartype == True:
 		kappa_prior = np.array([1]).astype(float)
 		alpha_prior = np.array([3, 1, 0.5, 0.3, 0.1, 0.01]).astype(float)
 
-
 """CONTROL PARAMETERS"""
-simulation_max_dist = 40.0   			# max distance of path for simulation tests
+simulation_max_dist = 40.0  # max distance of path for simulation tests
 iterations = 100
-control_algo = 'RRT'      # choose either 'PI', 'RRT_star', 'PRM_star', 'RRT', 'PRM'
-sigma_epsilon = pi / 16         # Exploration noise in radians, 90 grad = 1,57
+control_algo = 'RRT'  # choose either 'PI', 'RRT_star', 'PRM_star', 'RRT', 'PRM'
+sigma_epsilon = pi / 16  # Exploration noise in radians, 90 grad = 1,57
 R_cost = 5 * np.ones(shape=(1, 1))  # Immediate control cost. This is not incorporated in sampling algorithms, though could be in the cost function.
 border_variance_penalty = 1000
 
 """Choose control parameters for PI algorithm"""
 set_sanity_check = True  # Calculates cost for the optimal path and plots the optimal path
-n_updates = 10  		# Control loop updates
-n_k = 10  				# Number of virtual roll-out pathes
-n_horizon = 10			 # Control horizon length in s
-N_horizon = 10  		# Number of discrete rollout points
+n_updates = 10  # Control loop updates
+n_k = 10  # Number of virtual roll-out pathes
+n_horizon = 10  # Control horizon length in s
+N_horizon = 10  # Number of discrete rollout points
 t_cstep = n_horizon / N_horizon  # Control horizon step size in s
 pi_parameters = (n_updates, n_k, n_horizon, N_horizon, t_cstep, sigma_epsilon, R_cost)
 
 """Choose control parameters for sampling control algorithms"""
-max_runtime = 0.25 					# Runtime for the sampling algorithm to end after. Typically takes .05 seconds more than this runtime.
-max_curvature = 1.0     			# maximum curvature of a path allowed for the robot
-growth = 2.0       					# distance that RRT algorithms will steer nearest node to new node
-min_dist = 2.0                      # minimum distance of paths that the control algorithm will consider. needed to be >0 as we don't want to consider not moving (will get error if set to <=0). also good to not be super small, to discourage taking greedily very short informative paths that get stuck
-obstacles = None  					# any obstacles in the field. currently only handles squares specified by x,y location and side length
+max_runtime = 0.20  # Runtime for the sampling algorithm to end after. Typically takes .05 seconds more than this runtime.
+max_curvature = 1.0  # maximum curvature of a path allowed for the robot
+growth = 2.0  # distance that RRT algorithms will steer nearest node to new node
+min_dist = 2.0  # minimum distance of paths that the control algorithm will consider. needed to be >0 as we don't want to consider not moving (will get error if set to <=0). also good to not be super small, to discourage taking greedily very short informative paths that get stuck
+obstacles = None  # any obstacles in the field. currently only handles squares specified by x,y location and side length
 RRT_params = (field_dim, max_runtime, max_curvature, growth, min_dist, obstacles)
 PRM_params = (field_dim, max_runtime, max_curvature, min_dist, obstacles)
 
-
 #################################################################################################
 """DEFINE GENERAL FUNCTIONS"""
+
+
 # Run the selected control algorithm
 def control_algorithm(start, u_optimal, gmrf_params, var_x, max_dist, plot):
 	if control_algo == 'RRT_star':
@@ -121,13 +122,16 @@ def control_algorithm(start, u_optimal, gmrf_params, var_x, max_dist, plot):
 	elif control_algo == 'PRM':
 		return PRM(start, PRM_params, gmrf_params, var_x, max_dist, plot)
 
-# AUV model
-def auv_dynamics(x_auv, u_auv, epsilon_a, delta_t, field_dim, set_border=True):
-	x_auv_out = np.zeros(shape=3)
-	x_auv_out[2] = x_auv[2] + u_auv * delta_t + epsilon_a * sqrt(delta_t)  # computes angle
-	x_auv_out[0] = x_auv[0] + v_auv * cos(x_auv_out[2]) * delta_t
-	x_auv_out[1] = x_auv[1] + v_auv * sin(x_auv_out[2]) * delta_t
 
+# AUV model
+def auv_dynamics(x_auv, u_auv, epsilon_a, delta_t, field_dim, x_auv_new=None, set_border=True):
+	x_auv_out = np.zeros(shape=3)
+	if control_algo == 'PI':
+		x_auv_out[2] = x_auv[2] + u_auv * delta_t + epsilon_a * sqrt(delta_t)  # computes angle
+		x_auv_out[0] = x_auv[0] + v_auv * cos(x_auv_out[2]) * delta_t
+		x_auv_out[1] = x_auv[1] + v_auv * sin(x_auv_out[2]) * delta_t
+	else:
+		x_auv_out = x_auv_new
 	if set_border == True:
 		# Prevent AUV from leaving the true field
 		if x_auv_out[0] < field_dim[0]:
